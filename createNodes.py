f1b9bb087b9c69198717f9387711f36d8353031a
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-11 00:34:35 -0600

    Add CreateNodes file

---
+++ createNodes.py
@@ -0,0 +1,10 @@
+nodes = int(input('How many nodes you have? '))
+filename = input('File number: ')
+
+with open(f'./coordinates/nodes-{filename}.py', 'w') as f:
+    f.write('nodeList = [\n')
+    for i in range(nodes):
+        x = input(f'Enter x coordinate for node {i+1}: ')
+        y = input(f'Enter y coordinate for node {i+1}: ')
+        f.write("{{'x': '{0}', 'y': '{1}'}},\n".format(x, y))
+    f.write(']')
