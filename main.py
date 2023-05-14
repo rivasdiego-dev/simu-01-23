b3d5236ba9502be537a253d5758c4d26c18ac924
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-14 11:08:31 -0600

    Add menu
    Created a menu for the calculation of locals and added proper implementation of all functions.

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 import importlib
 from getLocals import *
 
+# Dynamic import
 module_number = input("Enter the number of the node's list to import: ")
 nodeListModule = importlib.import_module(f'coordinates.nodes-{module_number}', package='.')
 
+# VARS declaration
 nodeList = nodeListModule.nodeList
+globalK = 0
+globalQ = 0
+localBs = []
+localKs = []
+connectivity_table = []
+
+# FUNC declaration 
+def display_menu():
+    print("\nMenu:")
+    print("1. Get locals from one element")
+    print("2. Get locals from N elements")
+    print("3. Quit")
+
+def one_element():
+    node1 = int(input("Enter local node 1: "))
+    node2 = int(input("Enter local node 2: "))
+    node3 = int(input("Enter local node 3: "))
+    print("\n\tLocal K:\n")
+    print_matrix(get_local_k(globalK, nodeList[node1-1], nodeList[node2-1], nodeList[node3-1]))
+    print("\n\tLocal B:\n")
+    print_matrix(get_local_b(globalQ, nodeList[node1-1], nodeList[node2-1], nodeList[node3-1]))
+
+# Main workflow
+
+globalK = float(input("Please, enter the global K value: "))
+globalQ = float(input("Please, enter the global Q value: "))
+
+display_menu()
+while True:
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        one_element()
+        
+    elif choice == "2":
+        print("Option 2 selected")
+        
+    elif choice == "3":
+        print("\nClosing program...")
+        break  
+    else:
+        print("Invalid choice. Please try again.")
+    display_menu()
